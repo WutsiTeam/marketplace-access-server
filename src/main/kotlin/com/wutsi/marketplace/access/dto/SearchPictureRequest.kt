6d0c7205606404dcf672ee2633570a0354ce47bc
wutsi <herve.tchepannou@wutsi.com>
2022-10-31 08:23:24 -0400

    update api

--- src/main/kotlin/com/wutsi/marketplace/access/dto/SearchPictureRequest.kt
+++ src/main/kotlin/com/wutsi/marketplace/access/dto/SearchPictureRequest.kt
@@ -1,5 +1,10 @@
 package com.wutsi.marketplace.access.dto
 
+import kotlin.Int
+import kotlin.Long
+import kotlin.String
+import kotlin.collections.List
+
 public data class SearchPictureRequest(
     public val pictureIds: List<Long> = emptyList(),
     public val productIds: List<Long> = emptyList(),
